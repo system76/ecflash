c8ab9406ca37945511a9c96ca139b8a02ced8c75
Jeremy Soller <jeremy@system76.com>
2020-01-02 20:02:32 -0700

    Add SPI follow struct

--- examples/isp.rs
+++ examples/isp.rs
@@ -17,21 +17,21 @@ pub enum Address {
     INDDR = 8,
 }
 
-pub trait Parallel {
-    /// Set the parallel address
+pub trait Debugger {
+    /// Set the debugger address
     fn address(&mut self, address: u8) -> Result<()>;
-    /// Read data from the parallel port
+    /// Read data from the debugger port
     fn read(&mut self, data: &mut [u8]) -> Result<usize>;
-    /// Write data to the parallel port
+    /// Write data to the debugger port
     fn write(&mut self, data: &[u8]) -> Result<usize>;
 
-    /// Read data at a parallel address
+    /// Read data at a debugger address
     fn read_at(&mut self, address: Address, data: &mut [u8]) -> Result<usize> {
         self.address(address as u8)?;
         self.read(data)
     }
 
-    /// Write data at a parallel address
+    /// Write data at a debugger address
     fn write_at(&mut self, address: Address, data: &[u8]) -> Result<usize> {
         self.address(address as u8)?;
         self.write(data)
@@ -58,30 +58,49 @@ pub trait Parallel {
         self.flash_address(address)?;
         self.write_at(Address::INDDR, data)
     }
+}
+
+pub struct SpiFollow<'a, T: Debugger> {
+    port: &'a mut T,
+    eflash: bool,
+}
+
+impl<'a, T: Debugger> SpiFollow<'a, T> {
+    pub fn new(port: &'a mut T, eflash: bool) -> Result<Self> {
+        let mut spi = Self { port, eflash };
+        spi.reset()?;
+        Ok(spi)
+    }
 
     /// Disable SPI chip - should be done before and after each transaction
-    fn spi_reset(&mut self, eflash: bool) -> Result<()> {
-        self.flash_write(
-            if eflash { 0x7FFF_FE00 } else { 0xFFFF_FE00 },
+    pub fn reset(&mut self) -> Result<()> {
+        self.port.flash_write(
+            if self.eflash { 0x7FFF_FE00 } else { 0xFFFF_FE00 },
             &[0]
         )?;
         Ok(())
     }
 
     /// Read from SPI chip directly using follow mode
-    fn spi_read(&mut self, eflash: bool, data: &mut [u8]) -> Result<usize> {
-        self.flash_read(
-            if eflash { 0x7FFF_FD00 } else { 0xFFFF_FD00 },
+    pub fn read(&mut self, data: &mut [u8]) -> Result<usize> {
+        self.port.flash_read(
+            if self.eflash { 0x7FFF_FD00 } else { 0xFFFF_FD00 },
             data
         )
     }
 
     /// Write to SPI chip directly using follow mode
-    fn spi_write(&mut self, eflash: bool, data: &[u8]) -> Result<usize> {
-        self.flash_write(
-            if eflash { 0x7FFF_FD00 } else { 0xFFFF_FD00 },
+    pub fn write(&mut self, data: &[u8]) -> Result<usize> {
+        self.port.flash_write(
+            if self.eflash { 0x7FFF_FD00 } else { 0xFFFF_FD00 },
             data
         )
+    }
+}
+
+impl<'a, T: Debugger> Drop for SpiFollow<'a, T> {
+    fn drop(&mut self) {
+        let _ = self.reset();
     }
 }
 
@@ -92,7 +111,6 @@ pub struct ParallelArduino {
 impl ParallelArduino {
     /// Connect to parallel port arduino using provided port
     pub fn new<P: AsRef<Path>>(path: P) -> Result<Self> {
-        //TODO: do not unwrap
         let tty = TTYPort::open(path.as_ref(), &SerialPortSettings {
             baud_rate: 1000000,
             data_bits: serialport::DataBits::Eight,
@@ -113,50 +131,57 @@ impl ParallelArduino {
 
     fn ack(&mut self) -> Result<()> {
         let mut b = [0];
-        self.tty.read(&mut b)?;
+        self.tty.read_exact(&mut b)?;
         if b[0] == b'\r' {
             Ok(())
         } else {
             Err(Error::new(
                 ErrorKind::InvalidInput,
-                format!("received {:02X} instead of {:02X}", b[0], b'K')
+                format!("received ack of {:02X} instead of {:02X}", b[0], b'K')
             ))
         }
     }
 
     fn echo(&mut self) -> Result<()> {
-        self.tty.write(&[b'E'])?;
+        self.tty.write_all(&[b'E', 0, 0x76])?;
+        let mut b = [0];
+        self.tty.read_exact(&mut b)?;
+        if b[0] != 0x76 {
+            return Err(Error::new(
+                ErrorKind::InvalidInput,
+                format!("received echo of {:02X} instead of {:02X}", b[0], 0x76)
+            ));
+        }
         self.ack()?;
         Ok(())
     }
 }
 
-impl Parallel for ParallelArduino {
+impl Debugger for ParallelArduino {
     fn address(&mut self, address: u8) -> Result<()> {
-        self.tty.write(&[b'A', address])?;
+        self.tty.write_all(&[b'A', 0, address])?;
         self.ack()?;
         Ok(())
     }
 
     fn read(&mut self, data: &mut [u8]) -> Result<usize> {
-        let mut i = 0;
-        while i < data.len() {
-            self.tty.write(&[b'R'])?;
-            self.tty.read(&mut data[i..i+1])?;
+        for chunk in data.chunks_mut(256) {
+            let length = chunk.len().checked_sub(1).unwrap();
+            self.tty.write_all(&[b'R', length as u8])?;
+            self.tty.read_exact(chunk)?;
             self.ack()?;
-            i+=1;
         }
-        Ok(i)
+        Ok(data.len())
     }
 
     fn write(&mut self, data: &[u8]) -> Result<usize> {
-        let mut i = 0;
-        while i < data.len() {
-            self.tty.write(&[b'W', data[i]])?;
+        for chunk in data.chunks(256) {
+            let length = chunk.len().checked_sub(1).unwrap();
+            self.tty.write_all(&[b'W', length as u8])?;
+            self.tty.write_all(chunk)?;
             self.ack()?;
-            i+=1;
         }
-        Ok(i)
+        Ok(data.len())
     }
 }
 
@@ -175,15 +200,18 @@ fn isp() -> Result<()> {
 
     println!("ID: {:02X}{:02X} VER: {}", id[0], id[1], id[2]);
 
-    // Read signature
-    let mut buf = vec![0; 1024];
-    port.spi_reset(true)?;
-    port.spi_write(true, &[0x0B, 0x00, 0x00, 0x00, 0x00])?;
-    port.spi_read(true, &mut buf)?;
-    for i in 0x50..0x60 {
-        println!("{:02X}", buf[i]);
+    // Read entire ROM
+    {
+        let mut spi = SpiFollow::new(&mut port, true)?;
+
+        let mut buf = vec![0; 128 * 1024];
+        spi.write(&[0x0B, 0x00, 0x00, 0x00, 0x00])?;
+        spi.read(&mut buf)?;
+        // Print signature
+        for i in 0x50..0x60 {
+            println!("{:02X}", buf[i]);
+        }
     }
-    port.spi_reset(true)?;
 
     Ok(())
 }
