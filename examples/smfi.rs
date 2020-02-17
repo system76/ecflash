945ec8d276edd78f26cd744624344e67549824ad
Jeremy Soller <jeremy@system76.com>
2020-02-17 13:28:39 -0700

    Add console reading in smfi example

--- examples/smfi.rs
+++ examples/smfi.rs
@@ -1,5 +1,5 @@
 use hwio::{Io, Pio};
-use std::{io, process};
+use std::{io, process, thread, time};
 
 #[repr(u8)]
 enum SmfiCmd {
@@ -27,9 +27,17 @@ unsafe fn set(offset: u8, value: u8) {
     ).write(value)
 }
 
+unsafe fn dbg(offset: u8) -> u8 {
+    Pio::<u8>::new(
+        0xD00 + (offset as u16)
+    ).read()
+}
+
 unsafe fn smfi() {
     set(0, SmfiCmd::Probe as u8);
-    while get(0) != SmfiCmd::None as u8 {}
+    while get(0) != SmfiCmd::None as u8 {
+        thread::sleep(time::Duration::from_millis(1));
+    }
     if get(1) == SmfiRes::Ok as u8 {
         let signature = (get(2), get(3));
         let protocol = get(4);
@@ -42,7 +50,9 @@ unsafe fn smfi() {
     }
 
     set(0, SmfiCmd::Board as u8);
-    while get(0) != SmfiCmd::None as u8 {}
+    while get(0) != SmfiCmd::None as u8 {
+        thread::sleep(time::Duration::from_millis(1));
+    }
     if get(1) == SmfiRes::Ok as u8 {
         print!("board ");
         for i in 2..=255 {
@@ -56,7 +66,9 @@ unsafe fn smfi() {
     }
 
     set(0, SmfiCmd::Version as u8);
-    while get(0) != SmfiCmd::None as u8 {}
+    while get(0) != SmfiCmd::None as u8 {
+        thread::sleep(time::Duration::from_millis(1));
+    }
     if get(1) == SmfiRes::Ok as u8 {
         print!("version ");
         for i in 2..=255 {
@@ -67,6 +79,21 @@ unsafe fn smfi() {
             print!("{}", b as char);
         }
         println!();
+    }
+
+    let mut head = dbg(0) as usize;
+    loop {
+        let tail = dbg(0) as usize;
+        if tail == 0 || head == tail {
+            thread::sleep(time::Duration::from_millis(1));
+        } else {
+            while head != tail {
+                head += 1;
+                if head >= 256 { head = 1; }
+                let c = dbg(head as u8);
+                print!("{}", c as char);
+            }
+        }
     }
 }
 
