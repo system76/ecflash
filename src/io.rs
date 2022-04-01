b78471989a7ced11e574d1f4e1ec668b188a5b03
Tim Crawford <tcrawford@system76.com>
2022-04-01 12:53:53 -0600

    Use asm from core::arch
    asm was stabilized in Rust 1.59.0. It is now in `core::arch` and not in
    the prelude.
    
    Fixes compiling using the 2022-03-18 toolchain.
    
    Signed-off-by: Tim Crawford <tcrawford@system76.com>

--- src/io.rs
+++ src/io.rs
@@ -1,3 +1,5 @@
+use core::arch::asm;
+
 #[inline(always)]
 pub unsafe fn inb(port: u16) -> u8 {
     let value: u8;
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 #![no_std]
-#![feature(asm)]
 
 #[macro_use]
 extern crate alloc;
