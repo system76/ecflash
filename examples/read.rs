32da525f2e50ee6ba3501b06bcb881583f8bde8e
Garret Kelly <gkelly@gkel.ly>
2022-07-13 22:54:03 -0400

    Apply formatting to the source tree
    Signed-off-by: Garret Kelly <gkelly@gkel.ly>

--- examples/read.rs
+++ examples/read.rs
@@ -4,14 +4,17 @@ use ecflash::{EcFlash, Flasher};
 use std::{fs, io, process};
 
 fn main() {
-    extern {
+    extern "C" {
         fn iopl(level: isize) -> isize;
     }
 
     // Get I/O Permission
     unsafe {
         if iopl(3) < 0 {
-            eprintln!("Failed to get I/O permission: {}", io::Error::last_os_error());
+            eprintln!(
+                "Failed to get I/O permission: {}",
+                io::Error::last_os_error()
+            );
             process::exit(1);
         }
 
@@ -20,7 +23,7 @@ fn main() {
         let mut flasher = Flasher::new(ec);
 
         if flasher.start() == Ok(51) {
-            if let Ok(data) = flasher.read(|x| { eprint!("\r{} KB", x / 1024) }) {
+            if let Ok(data) = flasher.read(|x| eprint!("\r{} KB", x / 1024)) {
                 eprintln!();
                 let _ = fs::write("read.rom", data);
             } else {
